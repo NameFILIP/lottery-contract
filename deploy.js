205b83a2b029a0487caff58bddb0e73d7356b4fa
Filip Spiridonov <filip.spiridonov@gmail.com>
2021-09-25 16:05:06 +0300

    Fix the deploy script

--- deploy.js
+++ deploy.js
@@ -1,23 +1,33 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const contractFile = require("./compile.js");
 
-const provider = new HDWalletProvider(
-  "xxx",
-  "yyy"
-);
+const mnemonicPhrase =
+  "xxx";
+
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonicPhrase
+  },
+  providerOrUrl:
+    "yyy",
+});
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
 
   console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web.eth.Contract(contractFile.abi)
-    .deploy({ data: contractFile.evm.bytecode.object })
-    .send({ from: accounts[0], gas: "1000000" });
+  const result = await new web3.eth.Contract(contractFile.abi)
+    .deploy({ data: `0x${contractFile.evm.bytecode.object}` })
+    .send({
+      from: accounts[0],
+      gas: 4700000,
+    });
 
+  console.log("Interface", contractFile.abi);
   console.log("Contract deployed to", result.options.address);
 };
 
